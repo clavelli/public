e9ec9b6c21d15dfc9d622b6d83138be13c2a0866
Jason Clavelli <jasonclavelli@gmail.com>
2025-08-05 12:24:52 -0700

    Update

--- x.py
+++ x.py
@@ -13,7 +13,7 @@ operator_dunder_fns = [
     "mod",
     "divmod",
     "pow",
-    "radd",
+    "radd",  # eg 3 + X
     "rsub",
     "rmul",
     "rtruediv",
@@ -35,46 +35,89 @@ operator_dunder_fns = [
     "pos",
     "abs",
     "invert",
+    "len",  # Doesn't handle len(X) like you'd expect, unfortunately. But X.__len__ works.
 ]
 
 
-def build_capture_fn(name):
+def _build_operator_capture_fn(name):
     def f(self, *args):
-        return self.__class__(self._xcls_steps + [lambda v: getattr(v, name)(*args)])
+        # X + 1 means capture the .__add__ attr on X, then capture the call on it with arg 1.
+        return self._xcls_capture_attr(name)._xcls_capture_call(args, {})
 
     return f
 
 
-class XMeta(type):
+class _XClsMeta(type):
     # Ideally we could implement these in __getattr__ or by using __setattr__ in
     # __init__, but something about the implementation of operators makes it so it only
-    # counts if the method is actually defined on the class, and this is the only way
+    # counts if the method is actually defined on the class, and metaclass is the only way
     # to do that without typing everything a bunch of times.
     def __new__(cls, name, bases, dct):
         for x in operator_dunder_fns:
-            dct[f"__{x}__"] = build_capture_fn(f"__{x}__")
+            dct[f"__{x}__"] = _build_operator_capture_fn(f"__{x}__")
         return super().__new__(cls, name, bases, dct)
 
 
-class XCls(metaclass=XMeta):
-    def __init__(self, _xcls_steps=None):
-        self._xcls_steps = _xcls_steps or []
+class _XCls(metaclass=_XClsMeta):
+    def __init__(self, n_args, steps):
+        self._xcls_n_args = n_args
+        self._xcls_steps = steps
 
     def __call__(self, *args, **kwargs):
-        return XCls(self._xcls_steps + [lambda v: v(*args, **kwargs)])
+        return self._xcls_capture_call(args, kwargs)
 
     def __getattr__(self, name):
-        return XCls(self._xcls_steps + [lambda v: getattr(v, name)])
+        return self._xcls_capture_attr(name)
 
-    @property
-    def l(self):
-        def f(x):
-            value = x
-            for step in self._xcls_steps:
-                value = step(value)
-            return value
+    def l(self, *args):
+        assert len(args) == self._xcls_n_args
+        return self._xcls_resolve(
+            args[0], args[1] if len(args) > 1 else None, args[2] if len(args) > 2 else None
+        )
 
-        return f
+    def _xcls_clone(self, step):
+        return _XCls(self._xcls_n_args, self._xcls_steps + [step])
+
+    def _xcls_capture_call(self, args, kwargs):
+        # Do this up here instead of in the lambda to minimize work we're doing per call
+        arg_indexes_to_sub = set()
+        kwarg_keys_to_sub = set()
+        for i, a in enumerate(args):
+            if isinstance(a, self.__class__):
+                assert a._xcls_n_args == self._xcls_n_args
+                arg_indexes_to_sub.add(i)
+        for k, v in kwargs.items():
+            if isinstance(v, self.__class__):
+                assert v._xcls_n_args == self._xcls_n_args
+                kwarg_keys_to_sub.add(k)
+
+        return self._xcls_clone(
+            # eg X2.fn(Y2.abc) needs Y2.abc to resolve before calling .fn
+            lambda x, y, z, v: v(
+                *[
+                    (a._xcls_resolve(x, y, z) if i in arg_indexes_to_sub else a)
+                    for i, a in enumerate(args)
+                ],
+                **{
+                    k: (a._xcls_resolve(x, y, z) if k in kwarg_keys_to_sub else a)
+                    for k, a in kwargs.items()
+                },
+            )
+        )
+
+    def _xcls_capture_attr(self, name):
+        return self._xcls_clone(lambda x, y, z, v: getattr(v, name))
+
+    def _xcls_resolve(self, x, y, z):
+        value = None
+        for step in self._xcls_steps:
+            value = step(x, y, z, value)
+        return value
 
 
-X = XCls()
+X = _XCls(1, [lambda x, y, z, v: x])
+X2 = _XCls(2, [lambda x, y, z, v: x])
+Y2 = _XCls(2, [lambda x, y, z, v: y])
+X3 = _XCls(3, [lambda x, y, z, v: x])
+Y3 = _XCls(3, [lambda x, y, z, v: y])
+Z3 = _XCls(3, [lambda x, y, z, v: z])
